5b14d468e78b9a0f2e7d3fa0ff29715127e6a2ad
anderleti <irmoesaobradesing@gmail.com>
2024-08-11 23:47:22 -0300

    Terceira entrega corrigida

--- backend/app.ts
+++ backend/app.ts
@@ -13,17 +13,18 @@ async function main() {
   await AppDataSource.initialize();
   const userRepository: Repository<User> = AppDataSource.getRepository(User);
 
-  // let passwordRegex = new RegExp("^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$");
   let emailRegex = new RegExp("^[a-z0-9.]+@[a-z0-9]+.[a-z]+.([a-z]+)?$");
+  let passwordRegex = new RegExp(`^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$`);
+  let error:String|null = null;
 
   // app.get("/", (req, res) => res.redirect("/users"));
 
   //show all users
   app.get("/users", async (req, res) => {
     const users = await userRepository.find();
-    if (users.length === 0) {
-      res.status(404).send("Nenhum usuário encontrado");
-      return;
+    if (users.length < 1) {
+      error = "Nenhum usuário encontrado."
+      res.status(400).json({error});
     } else {
       res.json(users);
     }
@@ -36,7 +37,8 @@ async function main() {
     if (user) {
       res.send(user);
     } else {
-      res.status(400).send("Usuário não encontrado");
+      error = "Usuário não encontrado."
+      res.status(400).json({error});
     }
   });
 
@@ -44,84 +46,96 @@ async function main() {
   app.post("/users/", async (req, res) => {
     const { username, email, password } = req.body;
     if (username && email && password) {
-      let error = null;
-
       switch (true) {
         case username.length < 5:
-          error = "Nome de usuário deve ter pelo menos 5 caracteres";
+          error = "Nome de usuário deve ter pelo menos 5 caracteres.";
           break;
 
         case !emailRegex.exec(email):
-          error = "Email inválido";
+          error = "Email inválido.";
           break;
 
-        // case !passwordRegex.exec(password):
-          // error =
-          //   "Senha inválida. Certifique-se de que a senha tenha pelo menos 8 caracteres, 1 número e 1 letra maiúscula.";
-          // break;
+        case !passwordRegex.exec(password):
+          error =
+            "Senha inválida. Certifique-se de que a senha tenha pelo menos 8 caracteres, 1 número e 1 letra maiúscula.";
+          break;
 
         default:
           // Se não houver erro, adiciona o usuário
           const newUser = req.body;
           await userRepository.save(newUser)
+          res.send("Usuário adicionado com sucesso.")
       }
 
       res.status(400).json({ error });
     } else {
-      res.status(400).send("Preencha todos os campos");
+      error = "Preencha todos os campos.";
+      res.status(400).json({ error });
     }
   });
 
   //update specific user
-  app.post("/users/:id", async (req, res) => {
+  app.put("/users/:id", async (req, res) => {
     const userId = parseInt(req.params.id);
+    let userToUpdate = await userRepository.findOneBy({id: userId});
     const { username, email, password } = req.body;
-    const user = await userRepository.findOneBy({id: userId});
 
-    if (user) {
-      let error = null;
-      let emailRegex = new RegExp("^[a-z0-9.]+@[a-z0-9]+.[a-z]+.([a-z]+)?$");
-      let passwordRegex = new RegExp("^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$");
+    const newData = {
+      id: userId,
+      username: username,
+      email: email,
+      password: password,
+    }
 
-      switch (true) {
-        case username.length < 5:
-          error = "Nome de usuário deve ter pelo menos 5 caracteres";
-          break;
-
-        case !emailRegex.exec(email):
-          error = "Email inválido";
-          break;
-
-        // case !passwordRegex.exec(password):
-        //   error =
-        //     "Senha inválida. Certifique-se de que a senha tenha pelo menos 8 caracteres, 1 número e 1 letra maiúscula.";
-        //   break;
-
-        default:
-          // Se não houver erro, atualiza o usuário
-          const userUpdate = req.body;
-          await userRepository.save(userUpdate)
-          return res.redirect("/users/");
+    if (userToUpdate) {
+      
+      if (username && email && password) {
+        switch (true) {
+          case username.length < 5:
+            error = "Nome de usuário deve ter pelo menos 5 caracteres.";
+            break;
+  
+          case !emailRegex.exec(email):
+            error = "Email inválido.";
+            break;
+  
+          case !passwordRegex.exec(password):
+            error =
+              "Senha inválida. Certifique-se de que a senha tenha pelo menos 8 caracteres, 1 número e 1 letra maiúscula.";
+            break;
+  
+          default:
+            // Se não houver erro, atualiza o usuário
+            await userRepository.save(newData)
+            return res.send("Usuário atualizado com sucesso.")
+        }
+  
+        // Retorna o erro encontrado
+        res.status(400).json({ error });
+      } else {
+        error = "Preencha todos os campos";
+        res.status(400).json({ error });
       }
 
-      // Retorna o erro encontrado
-      res.status(400).json({ error });
     } else {
-      res.status(404).json({ erro: "Usuário não encontrado" });
+      res.status(404).json({ erro: "Usuário não encontrado." });
     }
   });
 
   //delete specific user
-  app.get("/users/remove/:id", async (req, res) => {
+  app.delete("/users/:id", async (req, res) => {
     const userId = parseInt(req.params.id);
 
     const userToRemove = await userRepository.findOneBy({id: userId});
     
     if (userToRemove) {
+      console.log(userToRemove);
       await userRepository.remove(userToRemove);
-      res.redirect("/users/");
+      res.send("Usuário deletado com sucesso.")
+          .redirect("/users/");
     } else {
-      res.status(404).send("Usuário não encontrado");
+      error = "Usuário não encontrado.";
+      res.status(404).json({error});
     }
   });
 
@@ -135,35 +149,3 @@ async function main() {
 };
 
 main();
-
-// proximo passo ja ja
-// async function main(){
-//     await AppDataSource.initialize();
-//     const userRepository: Repository<User> = AppDataSource.getRepository(User);
-
-//     const menuOptions = [
-//         {
-//             name: 'Cadastrar novo usuário',
-//             value: '/users/add',
-//             description: 'add new user'
-//         },
-//         {
-//             name: 'Listar todos os usuários',
-//             value: '/users',
-//             description: 'list users'
-//         },
-//         {
-//             name: 'Atualizar um usuário',
-//             value: '/users',
-//             description: 'update user'
-//         },
-//         {
-//             name: 'Remover um usuário',
-//             value: '/users/remove',
-//             description: 'delete user'
-//         },
-
-//     ];
-
-//     const menu =
-// }
